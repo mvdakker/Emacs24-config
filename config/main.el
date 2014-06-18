2664f3f732ec60567fdfca4957b32d11d77d64cc
Martin van den Akker <m.vandenakker@gmail.com>
2014-06-18 10:39:49 +0200

    Add default user settings.

--- config/main.el
+++ config/main.el
@@ -1,1 +1,24 @@
+;; _____________________________________________________________________________
+;;                                                        Default user variables
+
+;; Set user name
+(unless (boundp 'user-full-name)
+  (setq user-full-name "unknown user")
+)
+
+;; Set user email address
+(unless (boundp 'user-mail-address)
+  (setq user-mail-address "unknown-email-address")
+)
+
+;; Set debugging mode
+(unless (boundp 'user-debug)
+  (setq user-debug nil)
+)
+
+;; Set verbose mode
+(unless (boundp 'user-verbose)
+  (setq user-verbose nil)
+)
+
 (load (concat emacs-config-path "config/install.el"))
