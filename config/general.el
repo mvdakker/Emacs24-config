2aeef4badc8682d639f6bd61a68d74f7a02c4958
Martin van den Akker <m.vandenakker@gmail.com>
2014-06-21 13:28:30 +0200

    Add hippie-expand

--- config/general.el
+++ config/general.el
@@ -29,3 +29,19 @@
 
   (ido-mode t)
 )
+
+
+;; _____________________________________________________________________________
+;;                                                                 Hippie expand
+
+(when use-hippie-expand
+  (setq hippie-expand-try-functions-list '(try-expand-dabbrev
+					   try-expand-dabbrev-all-buffers
+					   try-complete-file-name-partially
+					   try-complete-file-name
+					   try-expand-line
+					   try-complete-lisp-symbol-partially
+					   try-complete-lisp-symbol))
+  
+  (global-set-key (kbd "C-/") 'hippie-expand)
+)
