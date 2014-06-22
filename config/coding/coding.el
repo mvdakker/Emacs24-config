357eb84bc2c57db3b60b9eb1fd6a88d242c478d2
Martin van den Akker <m.vandenakker@gmail.com>
2014-06-22 22:00:45 +0200

    Add showing of matching parenthesis.

--- config/coding/coding.el
+++ config/coding/coding.el
@@ -1,1 +1,13 @@
 (load (concat user-coding-settings-path "version_control.el"))
+
+
+;; _____________________________________________________________________________
+;;                                                             Match parenthesis
+
+(show-paren-mode t)
+(setq show-paren-delay .5           ; how long to wait?
+      show-paren-style 'expression  ; highlight the whole expression
+)
+
+(global-set-key (kbd "C-M-[")   'backward-sexp)
+(global-set-key (kbd "C-M-]")   'forward-sexp)
