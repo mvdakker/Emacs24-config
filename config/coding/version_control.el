88da8c73dec2488705a402053533b721e9a6b058
Martin van den Akker <m.vandenakker@gmail.com>
2014-06-25 15:01:13 +0200

    Add method to sort words alphabetically.

--- config/coding/version_control.el
+++ config/coding/version_control.el
@@ -76,3 +76,17 @@
 
 (global-set-key (kbd "C-c y") 'duplicate-line)
 (global-set-key (kbd "C-c c") (lambda() (interactive) (duplicate-line t)))
+
+;; ________________________________________________________________________
+;;                                                               Sort words
+
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+   Prefixed with negative \\[universal-argument], sorts in reverse.
+
+   The variable `sort-fold-case' determines whether alphabetic case
+   affects the sort order.
+
+   See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
