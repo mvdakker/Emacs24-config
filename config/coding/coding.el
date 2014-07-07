c3c4b163cfc41a01d969ab360aaafdb0288662c5
Martin van den Akker <m.vandenakker@gmail.com>
2014-07-07 21:05:00 +0200

    Fancy line numbering.

--- config/coding/coding.el
+++ config/coding/coding.el
@@ -57,6 +57,22 @@
 (when use-line-numbering
   (require 'linum)
 
+  (unless window-system
+    (add-hook 'linum-before-numbering-hook
+              (lambda ()
+                (setq-local linum-format-fmt
+                            (let ((w (length (number-to-string
+                                              (count-lines (point-min) (point-max))))))
+                              (concat "%" (number-to-string w) "d"))))))
+
+  (defun linum-format-func (line)
+    (concat
+     (propertize (format linum-format-fmt line) 'face 'linum)
+     (propertize " " 'face 'linum)))
+
+  (unless window-system
+    (setq linum-format 'linum-format-func))
+
   (column-number-mode 1)
 
   (global-set-key (kbd "C-c n") 'linum-mode)
