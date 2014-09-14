89a3868478d0f2d5ef87915d8c3c6d23d9149212
Martin van den Akker <m.vandenakker@gmail.com>
2014-09-14 22:04:25 +0200

    Fix python settings by first loading elpy and jedi.

--- config/coding/lang-python.el
+++ config/coding/lang-python.el
@@ -9,60 +9,6 @@
 (add-to-list 'auto-mode-alist '("\\.ipy\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
-;; ________________________________________________________________________
-;;                                                              Indentation
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq py-indent-offset 4
-                  indent-tabs-mode nil
-                  comment-start "# "
-                  whitespace-line-column 80)
-            (highlight-indentation-mode 0)))
-
-(defun set-newline-and-indent ()
-  "Map the return key with newline-and-indent"
-  (interactive)
-  (local-set-key (kbd "RET") 'newline-and-indent))
-
-(add-hook 'python-mode-hook 'set-newline-and-indent)
-
-;; ________________________________________________________________________
-;;                                                            Misc settings
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (when use-line-numbering (linum-mode 1))
-            (show-paren-mode 1)
-            (setq ac-auto-start 3)))
-
-;; ________________________________________________________________________
-;;                                                     Renaming buffer name
-
-(defun python-rename-buffer ()
-  "Add a directory postfix to the buffer name to
-   distinguish from multiple buffers with the same filename.
-   This renames buffers with filenames that match '__init__.py',
-   'api.py' and 'setup.py'."
-  (interactive)
-  (when (and (buffer-file-name)
-	     (or (string-match ".*__init__.py" (buffer-file-name))
-		 (string-match ".*setup.py" (buffer-file-name))
-		 (string-match ".*api.py" (buffer-file-name))))
-    (setq parent-dir (file-name-nondirectory (directory-file-name (file-name-directory (buffer-file-name))))
-          new-buffer-name (concat (file-name-nondirectory (buffer-file-name)) " [" parent-dir "]" ))
-    (rename-buffer new-buffer-name t)))
-
-(add-hook 'python-mode-hook (function python-rename-buffer))
-
-;; ________________________________________________________________________
-;;                                         Highlight special label keywords
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (font-lock-add-keywords nil '(("\\<\\(TODO\\|FIXME\\|BUG\\|TEST\\):" 1 font-lock-warning-face t)
-                                          ("\\<\\(DEBUG\\)\\>" 1 font-lock-warning-face t)))
-))
 
 ;; _____________________________________________________________________________
 ;;                                                                          Elpy
@@ -101,6 +47,55 @@
                  )))
     )
 )
+
+;; ________________________________________________________________________
+;;                                                              Indentation
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq py-indent-offset 4
+                  indent-tabs-mode nil
+                  comment-start "# "
+                  whitespace-line-column 80)
+            (highlight-indentation-mode 0)
+            (local-set-key (kbd "RET") 'newline-and-indent)))
+
+;; ________________________________________________________________________
+;;                                                            Misc settings
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (when use-line-numbering (linum-mode 1))
+            (show-paren-mode 1)
+            (setq ac-auto-start 3)))
+
+;; ________________________________________________________________________
+;;                                                     Renaming buffer name
+
+(defun python-rename-buffer ()
+  "Add a directory postfix to the buffer name to
+   distinguish from multiple buffers with the same filename.
+   This renames buffers with filenames that match '__init__.py',
+   'api.py' and 'setup.py'."
+  (interactive)
+  (when (and (buffer-file-name)
+	     (or (string-match ".*__init__.py" (buffer-file-name))
+		 (string-match ".*setup.py" (buffer-file-name))
+		 (string-match ".*api.py" (buffer-file-name))))
+    (setq parent-dir (file-name-nondirectory (directory-file-name (file-name-directory (buffer-file-name))))
+          new-buffer-name (concat (file-name-nondirectory (buffer-file-name)) " [" parent-dir "]" ))
+    (rename-buffer new-buffer-name t)))
+
+(add-hook 'python-mode-hook (function python-rename-buffer))
+
+;; ________________________________________________________________________
+;;                                         Highlight special label keywords
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (font-lock-add-keywords nil '(("\\<\\(TODO\\|FIXME\\|BUG\\|TEST\\):" 1 font-lock-warning-face t)
+                                          ("\\<\\(DEBUG\\)\\>" 1 font-lock-warning-face t)))
+))
 
 ;; _____________________________________________________________________________
 ;;                                                                      Flycheck
