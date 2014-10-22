ffc8b35a9fa129a6d2c5e50abc71a3cbdcdd8950
Martin van den Akker <m.vandenakker@gmail.com>
2014-10-22 21:34:54 +0200

    Update of ECB mode
    - startup setting
    - key bindings

--- config/coding/coding.el
+++ config/coding/coding.el
@@ -189,18 +189,111 @@
 ;;                                                            Emacs Code Browser
 
 (when use-ecb
-  (setq ecb-examples-bufferinfo-buffer-name nil
-        ecb-tip-of-the-day nil)
-
-  ;; ______________________________________________________________________
-  ;;                                                               Semantic
   (semantic-mode 1)
+  (require 'semantic/ia)
+
+  (add-hook 'ecb-activate-hook (lambda () (ecb-toggle-compile-window -1)))
+
+  (setq ecb-tip-of-the-day nil
+        ecb-show-tip-of-the-day nil
+        ecb-windows-width 38
+        ecb-layout-name "leftright2"
+        ecb-compile-window-height 12
+        ecb-enlarged-compilation-window-max-height 0.5
+        ecb-basic-buffer-sync-delay 0.5
+        ecb-highlight-tag-with-point-delay 0.5
+        ecb-auto-expand-tag-tree-collapse-other 'always
+        ecb-kill-buffer-clears-history 'auto
+        ecb-compile-window-temporally-enlarge 'both
+        ecb-enlarged-compilation-window-max-height 'half
+        ecb-examples-bufferinfo-buffer-name nil
+        ecb-tip-of-the-day nil
+        ecb-key-map `("C-c e"
+                      (t "fh" ecb-history-filter)
+                      (t "fs" ecb-sources-filter)
+                      (t "fm" ecb-methods-filter)
+                      (t "fr" ecb-methods-filter-regexp)
+                      (t "ft" ecb-methods-filter-tagclass)
+                      (t "fc" ecb-methods-filter-current-type)
+                      (t "fp" ecb-methods-filter-protection)
+                      (t "fn" ecb-methods-filter-nofilter)
+                      (t "fl" ecb-methods-filter-delete-last)
+                      (t "ff" ecb-methods-filter-function)
+                      (t "p" ecb-nav-goto-previous)
+                      (t "n" ecb-nav-goto-next)
+                      (t "lc" ecb-change-layout)
+                      (t "lr" ecb-redraw-layout)
+                      (t "lw" ecb-toggle-ecb-windows)
+                      (t "lt" ecb-toggle-layout)
+                      (t "s" ecb-window-sync)
+                      (t "r" ecb-rebuild-methods-buffer)
+                      (t "a" ecb-toggle-auto-expand-tag-tree)
+                      (t "x" ecb-expand-methods-nodes)
+                      (t "h" ecb-show-help)
+                      (t "gl" ecb-goto-window-edit-last)
+                      (t "g1" ecb-goto-window-edit1)
+                      (t "g2" ecb-goto-window-edit2)
+                      (t "gc" ecb-goto-window-compilation)
+                      (t "gd" ecb-goto-window-directories)
+                      (t "gs" ecb-goto-window-sources)
+                      (t "gm" ecb-goto-window-methods)
+                      (t "gh" ecb-goto-window-history)
+                      (t "ga" ecb-goto-window-analyse)
+                      (t "gb" ecb-goto-window-speedbar)
+                      (t "md" ecb-maximize-window-directories)
+                      (t "ms" ecb-maximize-window-sources)
+                      (t "mm" ecb-maximize-window-methods)
+                      (t "mh" ecb-maximize-window-history)
+                      (t "ma" ecb-maximize-window-analyse)
+                      (t "mb" ecb-maximize-window-speedbar)
+                      (t "e" eshell)
+                      (t "o" ecb-toggle-scroll-other-window-scrolls-compile)
+                      (t "\\" ecb-toggle-compile-window)
+                      (t "/" ecb-toggle-compile-window-height)
+                      (t "," ecb-cycle-maximized-ecb-buffers)
+                      (t "." ecb-cycle-through-compilation-buffers)))
+
+  (defun usr/ecb-deactivate ()
+    "deactivate ecb and split emacs into 2 windows that contain 2 most recent buffers"
+    (interactive)
+    (ecb-deactivate)
+    (split-window-right)
+    (switch-to-next-buffer)
+    (other-window 1)
+  )
+
+  (defun usr/ecb-hide-ecb-windows ()
+    "hide ecb and then split emacs into 2 windows that contain 2 most recent buffers"
+    (interactive)
+    (ecb-hide-ecb-windows)
+    (split-window-right)
+    (switch-to-next-buffer)
+    (other-window 1)
+  )
+
+  (defun usr/ecb-show-ecb-windows ()
+    "show ecb windows and then delete all other windows except the current one"
+    (interactive)
+    (ecb-show-ecb-windows)
+    (delete-other-windows)
+    (ecb-toggle-compile-window -1)
+    (ecb-redraw-layout)
+   )
 
   ;; ______________________________________________________________________
   ;;                                                           Key bindings
-  (global-set-key (kbd "C-c e") 'ecb-minor-mode)
-)
 
+  (global-set-key (kbd "C-c C-e") 'ecb-minor-mode)
+  (global-set-key (kbd "C-*") 'usr/ecb-show-ecb-windows)
+  (global-set-key (kbd "C-(") 'usr/ecb-hide-ecb-windows)
+  (global-set-key (kbd "C-!") 'ecb-goto-window-edit1)
+  (global-set-key (kbd "C-@") 'ecb-goto-window-edit2)
+  (global-set-key (kbd "C-#") 'ecb-goto-window-methods)
+  (global-set-key (kbd "C-$") 'ecb-goto-window-compilation)
+  (global-set-key (kbd "C-%") 'ecb-cycle-through-compilation-buffers)
+  (global-set-key (kbd "C-^") 'ecb-goto-window-directories)
+
+)
 
 ;; _____________________________________________________________________________
 ;;                                                                      Flycheck
